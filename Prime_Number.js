78935e61a8bfd4129299f9fc890ba2b8d4b8256a
V-sukumar <thalasuku2001@gmail.com>
2022-12-06 18:41:28 +0530

    m1_prime_coded

--- Prime_Number.js
+++ Prime_Number.js
@@ -4,5 +4,14 @@ function prime(num){
         if(num%i==0){
             count++;
         }
+    }if(count==2){
+        return true;
+    }else{
+        return false;
     }
+}let ans=prime(13)
+if(ans==true){
+    console.log("It is prime");
+}else{
+    console.log("It is not prime");
 }
